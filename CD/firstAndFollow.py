407889c0800c13a442cd427f87e103291223b43d
Dhvani Thakkar <113112823+dhvanithakkar@users.noreply.github.com>
2025-02-18 09:19:27 +0530

    Update firstAndFollow.py
    Fixed error-> was not returning follow_list, was only printing it. Used it in extend then. Fixed that
    
    Takes first occurrence of variable in a rule in case of multiple occurrences, need to split and use the rule smartly. new_rule created earlier would do.

--- CD/firstAndFollow.py
+++ CD/firstAndFollow.py
@@ -72,8 +72,8 @@ def all_follows(grammar, start_variable):
     
     return follows
 
-start_variable = "X"
-grammar = {"X": ["ABC"], "A": ["a", EPSILON], "B": ["b", EPSILON], "C": ["c", "d"]}
+# start_variable = "X"
+# grammar = {"X": ["ABC"], "A": ["a", EPSILON], "B": ["b", EPSILON], "C": ["c", "d"]}
 # print(first(grammar, start_variable))
 
 
@@ -92,33 +92,53 @@ def in_RHS(variable: str, grammar) -> bool:
 # Cache follow here.
 def follow(variable, grammar, start_variable):
     follow_list = []
-    print(variable, end = ": ")
+    print(variable, ": ")
     if variable == start_variable:
         follow_list.append(DOLLAR)
 
+        # Ayaan does not like this condition. RHS dekh hi raha anyway. Fair.
         if not in_RHS(variable, grammar):
             print("NOT IN RHS, dollar only", follow_list)
             return follow_list
-    
+        
+    # "A": ["BC", "EFGH", "H"]
     for LHS, RHS in grammar.items():
         for rule in RHS:
             if variable in rule:
+                print("RULE FOUND:", LHS, "->", rule)
                 parts = rule.split(variable)
                 
-                if parts[1:] == [""]:
+                
+                if parts[-1] == "":
+                    print("END OF RULE, FOLLOW PARENT:", LHS)
                     follow_list.extend(follow(LHS, grammar, start_variable))
                 
-                else:
-                    new_rule = "".join(parts[1:])
+                else:                    
+                    # new_rule is a substring of the rule string where it is: FIRST OCCURENCE OF VARIABLE -> END
+                    # new_rule = "".join(parts[1:])
+                    first_occurence = rule.find(variable)
+                    new_rule =  rule[first_occurence + 1:]
+
                     f = first_of_rule(new_rule)
+                    print("FIRST OF:", new_rule, "IS", f)
+
                     if has_epsilon(f):
+                        print("EPSILON IN FIRST OF", new_rule, ": FOLLOWS :", LHS)
                         follow_list.extend(follow(LHS, grammar, start_variable))
                         f = ignore_epsilon(f)
                     follow_list.extend(f)
-    print(follow_list)
+    
+    follow_list = sorted(list(set(follow_list)))
+    print("FOLLOW OF:", variable, follow_list)
+    return follow_list
 
 start_variable = "A"
 grammar = {"A": ["BC", "EFGH", "H"], "B": ["b"], "C": ["c", EPSILON], "E": ["e", EPSILON], "F": ["CE"], "G": ["g"], "H": ["h", EPSILON]}
+
+# start_variable = 'S'
+# grammar = {'S':['ACB', 'CbB', 'Ba'], 'B':['g', EPSILON], 'A' : ['da', 'BC'], 'C': ['h', EPSILON]}
+
+
 # print(first(grammar, start_variable))
-# print(all_firsts(grammar))
+print(all_firsts(grammar))
 print(all_follows(grammar, start_variable))
